5f2bea56b60fa86557cbcac7814de5e70b5aa7b5
EmmaRyanAmechu <eamechu@protonmail.com>
2018-10-16 17:43:21 +0300

    Added Models, Factories, Migrations, Routes and Controllers

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Product API Routes
+Route::apiResource('/products', 'ProductController');
+
+//Review Routes
+Route::group(['prefix'=>'products'], function(){
+    Route::apiResource('/{product}/reviews','ReviewController');
+}); 
--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -22,6 +22,8 @@ return array(
     'App\\Http\\Middleware\\TrimStrings' => $baseDir . '/app/Http/Middleware/TrimStrings.php',
     'App\\Http\\Middleware\\TrustProxies' => $baseDir . '/app/Http/Middleware/TrustProxies.php',
     'App\\Http\\Middleware\\VerifyCsrfToken' => $baseDir . '/app/Http/Middleware/VerifyCsrfToken.php',
+    'App\\Models\\Product' => $baseDir . '/app/Models/Product.php',
+    'App\\Models\\Review' => $baseDir . '/app/Models/Review.php',
     'App\\Providers\\AppServiceProvider' => $baseDir . '/app/Providers/AppServiceProvider.php',
     'App\\Providers\\AuthServiceProvider' => $baseDir . '/app/Providers/AuthServiceProvider.php',
     'App\\Providers\\BroadcastServiceProvider' => $baseDir . '/app/Providers/BroadcastServiceProvider.php',
