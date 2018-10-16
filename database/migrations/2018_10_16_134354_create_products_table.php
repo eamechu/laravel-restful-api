ed81e80f3a06b86991ace9f51404ee6d3a646e00
EmmaRyanAmechu <eamechu@protonmail.com>
2018-10-16 18:03:39 +0300

    Migrations created for Products and Reviews Table

--- database/migrations/2018_10_16_134354_create_products_table.php
+++ database/migrations/2018_10_16_134354_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name')->fulltext();
+            $table->text('details')->fulltext();
+            $table->integer('price')->index();
+            $table->integer('stock')->index();
+            $table->integer('discount');
             $table->timestamps();
         });
     }
