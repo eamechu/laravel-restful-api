9fe5c73f12bd649886d2e17a4b32df62f2bb8967
EmmaRyanAmechu <eamechu@protonmail.com>
2018-10-16 19:09:32 +0300

    Added Product Resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Resources\Product\ProductResource;
 
 class ProductController extends Controller
 {
 
     public function index()
     {
-        //
+        $products = Product::all();
+        return $products;
     }
 
     public function store(Request $request)
@@ -20,7 +22,8 @@ class ProductController extends Controller
 
     public function show(Product $product)
     {
-        //
+        //return $product;
+        return new ProductResource($product);
     }
 
     public function update(Request $request, Product $product)
