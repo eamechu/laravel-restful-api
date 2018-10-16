254eb274e6a8ba129e7990c610a7e9b9e40db642
EmmaRyanAmechu <eamechu@protonmail.com>
2018-10-16 18:29:46 +0300

    Database Seeding for some Test Data with Faker

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'details' => $faker->paragraph,
+        'price' => $faker->numberBetween(5000, 180000),
+        'stock' => $faker->numberBetween(500, 2000),
+        'discount' => $faker->numberBetween(1, 10),
     ];
 });
